7445eca5e8a7f3b33315e0087d7b2358cff5f151
Sl33pySage <jasonwolf82@hotmail.com>
2025-03-06 10:26:29 -0600

    Creating server and listening

--- script.ts
+++ script.ts
@@ -2,3 +2,11 @@ import * as net from "net";
 
 let Server = net.createServer();
 Server.listen({ host: "127.0.0.1", port: 1234 });
+
+function newConn(socket: net.Socket): void {
+  console.log("New Connection", socket.remoteAddress, socket.remotePort);
+}
+
+let server = net.createServer();
+server.on("connection", newConn);
+server.listen({ host: "127.0.0.1", port: 1234 });
