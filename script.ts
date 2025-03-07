928dfb3e11a852a233c40657e4eb0805d4eb9dfc
Sl33pySage <jasonwolf82@hotmail.com>
2025-03-07 08:19:56 -0600

    working on step 4

--- script.ts
+++ script.ts
@@ -1,16 +1,22 @@
 import { error } from "console";
 import * as net from "net";
 
-let Server = net.createServer();
-Server.listen({ host: "127.0.0.1", port: 1234 });
+let server = net.createServer();
+server.listen({ host: "127.0.0.1", port: 1234 });
 
 function newConn(socket: net.Socket): void {
   console.log("New Connection", socket.remoteAddress, socket.remotePort);
 }
 
-let server = net.createServer();
+server = net.createServer();
 server.on("connection", newConn);
 server.listen({ host: "127.0.0.1", port: 1234 });
-server.on("error", (err: Error) => {
-  throw error;
+
+// server.on("error", (err: Error) => {
+//   throw error;
+// });
+
+server.on("end", () => {
+  //FIN recieved. The connection will be closed automatically.
+  console.log("EOF.");
 });
