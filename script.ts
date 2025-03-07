8b47519aebef020c8511b129bddc9943494e5c09
Sl33pySage <jasonwolf82@hotmail.com>
2025-03-07 09:41:31 -0600

    Bug fixes and re organizing

--- script.ts
+++ script.ts
@@ -1,22 +1,27 @@
-import { error } from "console";
 import * as net from "net";
-
-let server = net.createServer();
-server.listen({ host: "127.0.0.1", port: 1234 });
 
 function newConn(socket: net.Socket): void {
   console.log("New Connection", socket.remoteAddress, socket.remotePort);
+
+  socket.on("end", () => {
+    //FIN recieved. The connection will be closed automatically.
+    console.log("EOF.");
+  });
+  socket.on("data", (data: Buffer) => {
+    console.log("Data: ", data);
+    socket.write(data); // Echo back the data.
+
+    // Actively closes the connection if the data contains 'q'
+    if (data.includes("q")) {
+      console.log("Closing connection...");
+      socket.end(); // This will send FIN and close the connection.
+    }
+  });
 }
 
-server = net.createServer();
+let server = net.createServer();
+server.on("error", (err: Error) => {
+  throw err;
+});
 server.on("connection", newConn);
 server.listen({ host: "127.0.0.1", port: 1234 });
-
-// server.on("error", (err: Error) => {
-//   throw error;
-// });
-
-server.on("end", () => {
-  //FIN recieved. The connection will be closed automatically.
-  console.log("EOF.");
-});
