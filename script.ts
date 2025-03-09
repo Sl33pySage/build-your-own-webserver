734fe783f6c9cb911b1cff994cce1747106c0e48
Sl33pySage <jasonwolf82@hotmail.com>
2025-03-08 18:12:38 -0600

    finished previous steps all the way to step 4 of chapter 4 and some various bug fixes

--- script.ts
+++ script.ts
@@ -19,7 +19,9 @@ function newConn(socket: net.Socket): void {
   });
 }
 
-let server = net.createServer();
+const server = net.createServer({
+  pauseOnConnect: true, // Required by 'TCPConn'
+});
 server.on("error", (err: Error) => {
   throw err;
 });
@@ -33,9 +35,69 @@ function soWrite(conn: TCPConn, data: Buffer): Promise<void>;
 type TCPConn = {
   // the JS Socket Object
   socket: net.Socket;
+  // from the 'error' event
+  err: null | Error;
+  // EOF, from the 'end' event
+  ended: boolean;
   // the callbacks of a promise of the current read
   reader: null | {
     resolve: (value: Buffer) => void;
     reject: (reason: Error) => void;
   };
 };
+
+// Create a wrapper from net.Socket
+function soInit(socket: net.Socket): TCPConn {
+  const conn: TCPConn = {
+    socket: socket,
+    err: null,
+    ended: false,
+    reader: null,
+  };
+
+  socket.on("data", (data: Buffer) => {
+    console.assert(conn.reader);
+    //pause the 'data' event until the next read.
+    conn.socket.pause();
+    //fulfill the promise of the current read.
+    conn.reader!.resolve(data);
+    conn.reader = null;
+  });
+  socket.on("end", () => {
+    // this also fulfills the current read.
+    conn.ended = true;
+    if (conn.reader) {
+      conn.reader.resolve(Buffer.from("")); // EOF
+      conn.reader = null;
+    }
+  });
+  socket.on("error", (err: Error) => {
+    // errors are also delivered to the current read.
+    conn.err = err;
+    if (conn.reader) {
+      conn.reader.reject(err);
+      conn.reader = null;
+    }
+  });
+  return conn;
+}
+
+// Returns an empty 'Buffer' after EOF.
+function soRead(conn: TCPConn): Promise<Buffer> {
+  console.assert(!conn.reader); // no concurrent calls
+  return new Promise((resolve, reject) => {
+    // If the connection is not readable, complete the promise now.
+    if (conn.err) {
+      reject(conn.err);
+      return;
+    }
+    if (conn.ended) {
+      resolve(Buffer.from("")); // EOF
+      return;
+    }
+    // Save the promise callbacks.
+    conn.reader = { resolve: resolve, reject: reject };
+    // and resume the 'data' event to fulfill the promise later.
+    conn.socket.resume();
+  });
+}
