cb3dea02d2afd1750f41575abac24e6ed165057c
Sl33pySage <jasonwolf82@hotmail.com>
2025-03-10 02:06:40 -0500

    worked through to step 4.4

--- script.ts
+++ script.ts
@@ -101,3 +101,21 @@ function soRead(conn: TCPConn): Promise<Buffer> {
     conn.socket.resume();
   });
 }
+
+function soWrite(conn: TCPConn, data: Buffer): Promise<void> {
+  console.assert(data.length > 0);
+  return new Promise((resolve, reject) => {
+    if (conn.err) {
+      reject(conn.err);
+      return;
+    }
+
+    conn.socket.write(data, (err?: Error) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    });
+  });
+}
